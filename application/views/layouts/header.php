a3ddd5fa8d5789b5615e70e2f7e0911f69a074c3
TEEJAY <forum.providence@yahoo.com>
2011-12-21 14:24:34 -0500

    theme and migration

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -1,5 +1,6 @@
 <html>
   <head><title>Marzan</title></head>
+  <link href="/css/overcast/jquery-ui-1.8.16.custom.css"></link>
 <body>
 <div id="header">
   Marzan
--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,16 +1,18 @@
 <?php load_header(); ?>
-<?php echo validation_errors();?>
-<?php echo form_open() ?>
-  <p>
-    <label>Username</label>
-    <input type="text" name="username" value="<?php echo set_value('username');?>" />
-  </p>
-  <p>
-    <label>Password</label>
-    <input type="password" name="password" value="" />
-  </p>
-  <p>
-    <input type="submit" value"Sign In" />
-  </p>
-</form>
+<div class='ui-widget'>
+  <?php echo validation_errors();?>
+  <?php echo form_open() ?>
+    <p>
+      <label>Username</label>
+      <input type="text" name="username" value="<?php echo set_value('username');?>" />
+    </p>
+    <p>
+      <label>Password</label>
+      <input type="password" name="password" value="" />
+    </p>
+    <p>
+      <input type="submit" value"Sign In" />
+    </p>
+  </form>
+</div>
 <?php load_footer(); ?>
